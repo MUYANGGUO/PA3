fdb5270c94b315e2b5ac0475f0381f7bc097060b
Yibo <wangyiboucr@gmail.com>
2020-04-22 11:53:52 -0400

    Update mpi_jacobi.cpp

--- Submission/mpi_jacobi.cpp
+++ Submission/mpi_jacobi.cpp
@@ -276,71 +276,6 @@ void distributed_matrix_vector_mult(const int n, double* local_A, double* local_
 
 }
 
-// Solves Ax = b using the iterative jacobi method
-void distributed_jacobi(const int n, double* local_A, double* local_b, double* local_x,
-                MPI_Comm comm, int max_iter, double l2_termination)
-{
-    //functions: MPI_Comm_rank, MPI_Cart_coords, MPI_Cart_rank
-
-    //get current&root rank and current&root position
-    int curRank, rootRank, diagRank;
-    int curPos[2], rootPos[2], diagPos[2];
-    rootPos[0] = 0, rootPos[1] = 0;
-    MPI_Comm_rank(comm, &curRank);
-    MPI_Cart_coords(comm, curRank, 2, curPos);
-    MPI_Cart_rank(comm, rootPos, &rootRank);
-
-    diagPos[0] = curPos[0], diagPos[1] = curPos[0];
-    MPI_Cart_rank(comm, DiagCoord, &DiagRank);
-
-    //splite mpi_comm into rows and cols
-    MPI_Comm comm_row, comm_col;
-    MPI_Comm_split(comm, curPos[0], curPos[1], &comm_row);
-    MPI_Comm_split(comm, curPos[1], curPos[0], &comm_col);
-
-    //get R and D
-    int num_row = block_decompose(n, comm_row);
-    int num_col = block_decompose(n, comm_col);
-
-    double* R = new double[num_row * num_col]();
-    double* D = new double[num_col]();
-    
-    getRD_jacobi(R, D, num_row, num_col, loacl_A,
-                curRank, diagRank, curPos, diagPos, comm_row, comm_col, comm);
-
-    //update x until it converges or reaches to max iteration
-    double* local_Rx = new double[num_col];
-    double* local_Ax = new double[num_col]; 
-    double l2_norm_square, sub_l2_norm_square;
-
-    for (int i = 0; i < max_iter; i ++){
-        //calculate R*x and A*x
-        distributed_matrix_vector_mult(n, R, local_x, local_Rx, comm);
-        distributed_matrix_vector_mult(n, local_A, local_x, local_Ax, comm);
-
-        //update x in the first column
-        if (curPos[1] == 0){
-            for (int i = 0; i < num_col; i++){
-                local_x[i] = (local_b[i] - local_Rx[i])/D[i];
-            }
-        }
-
-        //detect termination using l2 norm
-        l2_norm_square = 0;
-        sub_l2_norm_square = 0;
-       
-        if (curPos[1] == 0){
-            //get sub l2_norm of this row
-            sub_l2_norm_square +=  pow(local_Ax[i] - local_b[i], 2);
-        }
-        MPI_Reduce(&sub_l2_norm_square, &l2_norm_square, 1, MPI_DOUBLE, MPI_SUM, 0, col_comm);
-
-        MPI_Bcast(&l2_norm_square, 1, MPI_DOUBLE, rootRank, comm);
-        if (sqrt(l2_norm_square) <= l2_termination) break;
-    }
-
-    
-}
 
 void getRD_jacobi(double* R, double* D, int num_row, int num_col, 
                 double* local_A, int curRank, int diagRank, 
@@ -374,6 +309,74 @@ void getRD_jacobi(double* R, double* D, int num_row, int num_col,
         MPI_Recv(D, num_col, MPI_DOUBLE, diagRank, 1, comm, MPI_STATUS_IGNORE);  
     }
 }
+
+// Solves Ax = b using the iterative jacobi method
+void distributed_jacobi(const int n, double* local_A, double* local_b, double* local_x,
+                MPI_Comm comm, int max_iter, double l2_termination)
+{
+    //functions: MPI_Comm_rank, MPI_Cart_coords, MPI_Cart_rank
+
+    //get current&root rank and current&root position
+    int curRank, rootRank, diagRank;
+    int curPos[2], rootPos[2], diagPos[2];
+    rootPos[0] = 0, rootPos[1] = 0;
+    MPI_Comm_rank(comm, &curRank);
+    MPI_Cart_coords(comm, curRank, 2, curPos);
+    MPI_Cart_rank(comm, rootPos, &rootRank);
+
+    diagPos[0] = curPos[0], diagPos[1] = curPos[0];
+    MPI_Cart_rank(comm, diagPos, &diagRank);
+
+    //splite mpi_comm into rows and cols
+    MPI_Comm comm_row, comm_col;
+    MPI_Comm_split(comm, curPos[0], curPos[1], &comm_row);
+    MPI_Comm_split(comm, curPos[1], curPos[0], &comm_col);
+
+    //get R and D
+    int num_row = block_decompose(n, comm_row);
+    int num_col = block_decompose(n, comm_col);
+
+    double* R = new double[num_row * num_col]();
+    double* D = new double[num_col]();
+    
+    getRD_jacobi(R, D, num_row, num_col, local_A,
+                curRank, diagRank, curPos, diagPos, comm_row, comm_col, comm);
+
+    //update x until it converges or reaches to max iteration
+    double* local_Rx = new double[num_col];
+    double* local_Ax = new double[num_col]; 
+    double l2_norm_square, sub_l2_norm_square;
+
+    for (int i = 0; i < max_iter; i ++){
+        //calculate R*x and A*x
+        distributed_matrix_vector_mult(n, R, local_x, local_Rx, comm);
+        distributed_matrix_vector_mult(n, local_A, local_x, local_Ax, comm);
+
+        //update x in the first column
+        if (curPos[1] == 0){
+            for (int i = 0; i < num_col; i++){
+                local_x[i] = (local_b[i] - local_Rx[i])/D[i];
+            }
+        }
+
+        //detect termination using l2 norm
+        l2_norm_square = 0;
+        sub_l2_norm_square = 0;
+       
+        if (curPos[1] == 0){
+            //get sub l2_norm of this row
+            sub_l2_norm_square +=  pow(local_Ax[i] - local_b[i], 2);
+        }
+        MPI_Reduce(&sub_l2_norm_square, &l2_norm_square, 1, MPI_DOUBLE, MPI_SUM, 0, comm_col);
+
+        MPI_Bcast(&l2_norm_square, 1, MPI_DOUBLE, rootRank, comm);
+        if (sqrt(l2_norm_square) <= l2_termination) break;
+    }
+
+    
+}
+
+
 
 
 // wraps the distributed matrix vector multiplication
